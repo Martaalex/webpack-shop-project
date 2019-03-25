a7054e76c599d794f2a88e791621ecc29c64efd6
Martaalex <marta.aleksandrovic@gmail.com>
2019-03-25 21:58:09 +0200

    redux devtools extension

--- shop-project-master/src/app/state/index.js
+++ shop-project-master/src/app/state/index.js
@@ -1,7 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import reducers from './reducers';
 import middlewares from './middleware';
 
-const store = createStore(reducers, applyMiddleware(...middlewares));
+const appliedMiddleware = applyMiddleware(...middlewares);
+const finalMiddlewares =
+  process.env.NODE_ENV === 'productions'
+    ? appliedMiddleware
+    : composeWithDevTools(appliedMiddleware);
+const store = createStore(reducers, finalMiddlewares);
 
 export default store;
